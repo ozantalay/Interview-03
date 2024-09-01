6898fce748872f54eeee3b2bb5090d6ae5f0aa0b
ozantalay <ozntalay@hotmail.com>
2024-09-01 19:50:37 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  return <GenerateList />;
+}
+
+const GenerateList = () => {
+  //KODUNUZ BURAYA GELECEK
+};
+
+const ExpandableListItem = ({ item }) => {
+  // KODUNUZ BURAYA GELECEK
+};
+
+export default App;
